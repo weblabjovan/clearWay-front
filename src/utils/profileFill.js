55d4429ea41fd4db6d0b9ea269438fd984c87ef2
weblabjovan <jovan.sutic@weblab.co.rs>
2018-07-10 22:36:19 +0200

    profile fill function upgrade

--- src/utils/profileFill.js
+++ src/utils/profileFill.js
@@ -1,14 +1,17 @@
 export default (user) => {
 	let result = { ... user};
-	if (user.car.model) {
-		result.model = user.car.model;
+	if (user.car) {
+		if (user.car.model) {
+			result.model = user.car.model;
+		}
+		if (user.car.modelYear) {
+			result.modelYear = user.car.modelYear;
+		}
+		if (user.car.modelNumber) {
+			result.modelNumber = user.car.modelNumber;
+		}
 	}
-	if (user.car.modelYear) {
-		result.modelYear = user.car.modelYear;
-	}
-	if (user.car.modelNumber) {
-		result.modelNumber = user.car.modelNumber;
-	}
+	
 	if (user.userType === 'false') {
 		result.userType = false;
 	}
